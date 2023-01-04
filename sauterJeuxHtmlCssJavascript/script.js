757b5212092707cf9ac3a07ad5ab5f0c0ab9c6b8
julien93130fr <120179983+julien93130fr@users.noreply.github.com>
2023-01-04 12:57:19 +0100

    Add files via upload

---
+++ sauterJeuxHtmlCssJavascript/script.js
@@ -0,0 +1,30 @@
+
+let perso = document.querySelector(".perso");
+let obstacles = document.querySelector(".obstacles");
+
+function sauter(){
+    // function pour faire sauter le personnage
+  if (perso.classList != "animation") {
+    perso.classList.add('animation')
+    
+  }
+  setTimeout(function(){
+  perso.classList.remove('animation');
+},500)
+
+}
+
+
+// vérifier si l' obstacle touche le personnage
+
+let verification = setInterval(function(){
+    let persoTop = parseInt(window.getComputedStyle(perso).getPropertyValue("top"));
+    let obstaclesLeft = parseInt(window.getComputedStyle(obstacles).getPropertyValue("left"));
+
+    if (obstaclesLeft<20 && obstaclesLeft> 0 && persoTop>= 130) {
+        obstacles.style.animation = "none";
+        alert("vous avez perdu")
+    }
+
+
+},1)    
